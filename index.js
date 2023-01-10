1dcb6575e4f214ef69c3b2f1571d048bd4a67b6b
Mateo-Rey <jrey3darts@gmail.com>
2023-01-10 09:14:18 -0500

    api connected to frontend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as dotenv from "dotenv";
 import express from 'express'
+import cors from 'cors';
+import bodyParser from "body-parser";
 dotenv.config();
 const configuration = new Configuration({
     organization: "org-EvVHU27T5orMvKehTCjNsoKV",
@@ -9,18 +11,23 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const port = 3080;
 
 app.post('/', async (req, res) => {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
-      });
-      console.log(response.data.choices[0].text)
+    const { message } = req.body;
+    console.log(message)
+    // const response = await openai.createCompletion({
+    //     model: "text-davinci-003",
+    //     prompt: "Say this is a test",
+    //     max_tokens: 7,
+    //     temperature: 0,
+    //   });
+    //   console.log(response.data.choices[0].text)
       res.json({
-        data: response.data
+        // data: response.data
+        data: message,
       })
 })
 
