85a6d1439b2e55b75cb5377d8f04ac8806a1d8f2
Mateo-Rey <jrey3darts@gmail.com>
2023-01-10 09:54:04 -0500

    accpeting front end input

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as dotenv from "dotenv";
-import express from 'express'
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 dotenv.config();
 const configuration = new Configuration({
-    organization: "org-EvVHU27T5orMvKehTCjNsoKV",
-    apiKey: process.env.OPENAI_SECRET_KEY,
+  organization: "org-EvVHU27T5orMvKehTCjNsoKV",
+  apiKey: process.env.OPENAI_SECRET_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -15,22 +15,21 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 3080;
 
-app.post('/', async (req, res) => {
-    const { message } = req.body;
-    console.log(message)
-    // const response = await openai.createCompletion({
-    //     model: "text-davinci-003",
-    //     prompt: "Say this is a test",
-    //     max_tokens: 7,
-    //     temperature: 0,
-    //   });
-    //   console.log(response.data.choices[0].text)
-      res.json({
-        // data: response.data
-        data: message,
-      })
-})
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
+
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
 
 app.listen(port, (req, res) => {
-    console.log("listening on port 3080")
-})
+  console.log("listening on port 3080");
+});
